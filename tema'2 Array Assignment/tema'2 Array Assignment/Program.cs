4bbf7f7458a831c5b9d290259e87f97b259d5c96
MorarMihai <150845969+MorarMihai@users.noreply.github.com>
2023-12-05 22:07:25 +0200

    tema 2 array assgnement

---
+++ tema'2 Array Assignment/tema'2 Array Assignment/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace tema_2_Array_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numbersOfNumbers = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[numbersOfNumbers];
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int biggestNumber = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (biggestNumber < array[i])
+                {
+                    biggestNumber = array[i];
+                }
+            }
+            Console.WriteLine("Cel mai mare numar este " + biggestNumber);
+            int smallestNumber = array[0];
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (smallestNumber > array[i])
+                {
+                    smallestNumber = array[i];
+                }
+            }
+            Console.WriteLine("Cel mai mic numar este " + smallestNumber);
+        }
+    }
+}
