238687a9b22629dffa21525f371f0aefea57606c
MorarMihai <150845969+MorarMihai@users.noreply.github.com>
2023-12-23 18:29:20 +0200

    TEMA ROT3 CIPHER

---
+++ TEMA ROT3 CIPHER/TEMA ROT3 CIPHER/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+//using System.Text;
+
+namespace TEMA_ROT3_CIPHER
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder cipher = new StringBuilder(Console.ReadLine());
+            StringBuilder alphabet = new StringBuilder ("abcdefghijklmnopqrstuvwxyz");
+            StringBuilder newCipher = new StringBuilder("");
+            for(int i = 0; i < cipher.Length; i++)
+            {
+                for (int j = 0; j < alphabet.Length; j++)
+                {
+                    if (cipher[i] == alphabet[j])
+                    {
+                        if(cipher[i] == 'x')
+                        {
+                            newCipher = newCipher.Append(alphabet[i]);
+                            break;
+                        }
+                        if(cipher[i] == 'y')
+                        {
+                            newCipher = newCipher.Append(alphabet[1]);
+                            break;
+                        }
+                        if(cipher[i] == 'z')
+                        {
+                            newCipher = newCipher.Append(alphabet[1 + 1]);
+                            break;
+                        }
+                        newCipher = newCipher.Append(alphabet[j + 3]);
+                        break;
+                    }
+                }             
+            }
+            Console.WriteLine(newCipher);
+        }
+    }
+}
