e2c653948cc0ec2450ccfaac96979cfc6a26336d
MorarMihai <150845969+MorarMihai@users.noreply.github.com>
2023-12-28 16:05:42 +0200

    TEMA PALINDROM

---
+++ TEMA PALINDROM/TEMA PALINDROM/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace TEMA_PALINDROM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string firstWord = "";
+            string secondWord = "";
+            firstWord = Console.ReadLine();
+            for (int i = firstWord.Length - 1; i >= 0; i--)
+            {
+                secondWord += firstWord[i];
+            }
+            if (secondWord == firstWord)
+            {
+                Console.WriteLine("Stringul este palindrom \n Stringul introdus a fost {0} si inversului lui este {1}", firstWord, secondWord);
+            }
+            else
+            {
+                Console.WriteLine("Stringul nu este palindrom \n Stringul introdus a fost {0} si inversului lui este {1} {1}", firstWord, secondWord);
+            }
+        }
+    }
+}
