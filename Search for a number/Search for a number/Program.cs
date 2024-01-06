9ee052110ba162f2d3dc1308b32260b5143e293b
MorarMihai <150845969+MorarMihai@users.noreply.github.com>
2024-01-06 19:17:00 +0200

    TEMA SEARCH FOR A NUMBER

---
+++ Search for a number/Search for a number/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Search_for_a_number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] cars = new string[] { "Volvo", "Audi", "Mercedes", "Mazda", "Ford", "Toyota" };
+            string input = Console.ReadLine();
+            bool answer = false;
+            for(int i = 0; i < cars.Length; i++)
+            {
+                if(input == cars[i])
+                {
+                    answer = true;
+                    Console.WriteLine(i);
+                    break;
+                }
+            }
+            if (answer == false)
+            {
+                Console.WriteLine("The car doesn't exist");
+            }
+            
+        }
+    }
+}
