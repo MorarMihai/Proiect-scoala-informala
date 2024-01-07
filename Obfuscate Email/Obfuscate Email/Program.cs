49b1243a5d319b66de0e2adbc550dae8a1239ca1
MorarMihai <150845969+MorarMihai@users.noreply.github.com>
2024-01-07 17:32:08 +0200

    OBFUSCATE EMAIL

---
+++ Obfuscate Email/Obfuscate Email/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+
+namespace Obfuscate_Email
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder email = new StringBuilder(Console.ReadLine());
+            int indexOfAt = -1;
+            for(int i = 0; i <email.Length; i++)
+            {
+                if(email[i] == '@')
+                {
+                    indexOfAt = i;
+                }
+            }
+            //Console.WriteLine(indexOfAt);
+            for(int i = 0; i < email.Length; i++)
+            {
+                if(indexOfAt > i )
+                {
+                    email[i] = '*';
+                }
+            }
+            Console.WriteLine(email);
+        }
+    }
+}
