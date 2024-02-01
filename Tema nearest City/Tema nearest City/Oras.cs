0db06ec7324648ee88c7019172c5f3a78680f644
MorarMihai <150845969+MorarMihai@users.noreply.github.com>
2024-02-01 23:02:16 +0200

    Tema Nearest City

---
+++ Tema nearest City/Tema nearest City/Oras.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_nearest_City
+{
+    class Oras
+    {
+        private int _firstCoordonate;
+        private int _secondCoordonate;
+        private string _cityName;
+
+        public Oras()
+        {
+
+        }
+        public Oras(int firstCoordonate,int secondCoordonate,string cityName)
+        {
+            _cityName = cityName;
+            _firstCoordonate = firstCoordonate;
+            _secondCoordonate = secondCoordonate;
+        }
+        public int FirstCoordonate
+        {
+            get { return _firstCoordonate; }
+            set { value = _firstCoordonate; }
+        }
+        public int SecondCoordonate
+        {
+            get { return _secondCoordonate; }
+            set { value = _secondCoordonate; }
+        }
+        public string CityName
+        {
+            get { return _cityName; }
+            set { value = CityName; }
+        }
+    }
+}
---
+++ Tema nearest City/Tema nearest City/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Tema_nearest_City
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Oras Huedin = new Oras(4, 7, "Huedin");
+            Oras Oradea = new Oras(2, 6, "Oradea");
+            Oras Arad = new Oras(3, 8, "Arad");
+            Oras Dej = new Oras(5, 3, "Dej");
+            Oras Brasov = new Oras(6, 9, "Brasov");
+
+            string input = Console.ReadLine();
+            Oras cityToCompare = new Oras();
+            Oras[] orase = { Huedin, Oradea, Arad, Dej, Brasov };
+            double smallestdistance = 0;
+            string nameOfNearestCity = string.Empty;
+
+            for (int i = 0; i < orase.Length; i++)
+            {
+
+                if (orase[i].CityName.Equals(input))
+                {
+                    cityToCompare = orase[i];
+                }
+            }
+
+            for (int i = 0; i < orase.Length; i++)
+            {
+                if (orase[i].CityName.Equals(cityToCompare.CityName))
+                {
+                    continue;
+                }
+
+                var currentDistance = Math.Sqrt(Math.Pow(cityToCompare.FirstCoordonate - orase[i].FirstCoordonate, 2) + Math.Pow(cityToCompare.SecondCoordonate - orase[i].SecondCoordonate, 2));
+
+                if(smallestdistance == 0)
+                {
+                    smallestdistance = currentDistance;
+                }
+
+                if (currentDistance <= smallestdistance)
+                {
+                    smallestdistance = currentDistance;
+                    nameOfNearestCity = orase[i].CityName;
+                }
+            }
+
+            Console.WriteLine("Cel mai apropiat oras este : " +  nameOfNearestCity);
+        }
+    }
+}
+
