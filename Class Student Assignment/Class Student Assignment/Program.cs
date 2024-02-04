46e8f826f1e4d2126a56ecb21b8661032741207b
MorarMihai <150845969+MorarMihai@users.noreply.github.com>
2024-02-05 00:18:23 +0200

    Tema Class Student Assignment

---
+++ Class Student Assignment/Class Student Assignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Class_Student_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student Adrian = new Student(20, "Adrian");
+            Console.WriteLine(Adrian.Info);
+            Student Ion = new Student(20, "Ion");
+            Student Vlad = new Student(24, "Vlad");
+            Student George = new Student(22, "George");
+            Student Daniel = new Student(21, "Daniel");
+            Console.WriteLine(Ion.Info);
+            Console.WriteLine(Vlad.Info);
+            Console.WriteLine(George.Info);
+            Console.WriteLine(Daniel.Info);
+            Console.WriteLine("Nota lui Daniel este : ");
+            Daniel.Mark = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nota lui Vlad este : ");         
+            Vlad.Mark = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Nota lui Ion este : ");
+            Ion.Mark = Convert.ToInt32(Console.ReadLine());
+            Student[] studentMarks = { Daniel, Vlad, Ion };
+            foreach (Student item in studentMarks)
+            {
+                Console.WriteLine("Media notelor celor doi este : " + item.GetAverageMark((double)Daniel.Mark,(double) Vlad.Mark,(double)Ion.Mark));
+                break;
+            }
+        }
+    }
+}
+
---
+++ Class Student Assignment/Class Student Assignment/Student.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Student_Assignment
+{
+    class Student
+    {
+        private int _age;
+        private string _name;
+        private const int MIN_AGE = 18;
+        private const int MAX_AGE = 99;
+
+        public Student(int age,string name)
+        {
+            _age = age;
+            _name = name;
+        }
+
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value >= MIN_AGE && value <= MAX_AGE)
+                {
+                    _age = value;
+                }
+            }
+        }
+        public int? Mark { get; set; }
+
+        public  dynamic Info
+        {
+            get { return $"{_name}, {_age} ani"; }
+            set { _age = value;_name = value; }
+        }
+        public double GetAverageMark(double a,double b,double c)
+        {
+            return (a + b + c) / 3;
+        }
+    }
+}
