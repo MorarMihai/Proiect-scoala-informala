4d878d35742adc3c6020b82265c6d41eac203152
MorarMihai <150845969+MorarMihai@users.noreply.github.com>
2024-02-23 18:50:55 +0200

    TEMA DOG ASSIGNMENT CU EXCEPTII

---
+++ Tema Dog assignment cu exceptii/Tema Dog assignment cu exceptii/Dog.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tema_Dog_assignment_cu_exceptii
+{
+    class Dog
+    {
+        private string _name;
+        private int _age;
+
+        public Dog()
+        {
+            _name = string.Empty;
+            _age = 0;
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                if (value.Length < 2)
+                {
+                    throw new ApplicationException("Nu fiti zgarciti cu literele,pune-ti un nume mai frumos cainelui!");
+                }
+
+                _name = value;
+            }
+        }
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Varsta aceasta nu exista!");
+                }
+                if (value > 100)
+                {
+                    throw new ApplicationException("Varsta aceasta este prea mare!");
+                }
+
+                _age = value;
+            }
+        }
+    }
+}
---
+++ Tema Dog assignment cu exceptii/Tema Dog assignment cu exceptii/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Tema_Dog_assignment_cu_exceptii
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog Max = new Dog();
+            
+            try
+            {
+                Max.Age = -8;
+
+            }
+            catch (ApplicationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            try
+            {
+                Max.Name = "P";
+
+            }
+            catch (ApplicationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
